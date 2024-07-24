755d301d7227e5ac14ed90317d117b871f0de17b
Brian Hernandez <46072690+bhern154@users.noreply.github.com>
2024-07-23 20:35:35 -0700

    Added comments

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 
 // 1. Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number. (Make sure you get back JSON by including the ***json*** query key, specific to this API. [Details](http://numbersapi.com/#json).
 
+// Call NumbersAPI to get facts about number 24
 axios.get("http://numbersapi.com/24#json")
 .then(res => {console.log(res.data)})
 .catch(err => { console.log("Error", err)})
 
 // 2. Figure out how to get data on multiple numbers in a single request. Make that request and when you get the data back, put all of the number facts on the page.
 
+// list of 5 URLs to get facts on 5 numbers from NumbersAPI
 let URL_List = [
     axios.get("http://numbersapi.com/24#json"), 
     axios.get("http://numbersapi.com/52#json"), 
@@ -16,8 +18,10 @@ let URL_List = [
     axios.get("http://numbersapi.com/48#json")
 ]
 
+// use Promise.all to call all urls calls at once
 Promise.all(URL_List)
 .then(allRes => {
+    // append the response to the website
     let $ul = $('<ul></ul>');
     for (const res of allRes){
         let $li = $('<li></li>').text(res.data);
@@ -30,6 +34,7 @@ Promise.all(URL_List)
 // 3. Use the API to get 4 facts on your favorite number. Once you have them all, put them on the page. It’s okay if some of the facts are repeats.
 //     *(Note: You’ll need to make multiple requests for this.)*
 
+// list of 4 urls to get facts about the same number from NumbersAPI
 let URL_List_2 = [
     axios.get("http://numbersapi.com/24#json"), 
     axios.get("http://numbersapi.com/24#json"), 
@@ -37,8 +42,10 @@ let URL_List_2 = [
     axios.get("http://numbersapi.com/24#json")
 ]
 
+// use Promise.all to call all urls calls at once
 Promise.all(URL_List_2)
 .then(allRes => {
+    // append the response to the website
     let $ul = $('<ul></ul>');
     for (const res of allRes){
         let $li = $('<li></li>').text(res.data);
@@ -52,8 +59,10 @@ Promise.all(URL_List_2)
 
 // 1. Make a request to the [Deck of Cards API](http://deckofcardsapi.com/) to request a single card from a newly shuffled deck. Once you have the card, ***console.log*** the value and the suit (e.g. “5 of spades”, “queen of diamonds”).
 
+// request a new deck from DeckOfCardsAPI and draw one card
 axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
 .then(res => { 
+    // log the card details
     let card = res.data.cards[0]
     console.log(`${card.value} OF ${card.suit}`)
 })
@@ -62,21 +71,25 @@ axios.get("https://deckofcardsapi.com/api/deck/new/draw/?count=1")
 // 2. Make a request to the deck of cards API to request a single card from a newly shuffled deck. Once you have the card, make a request to the same API to get one more card from the **same** deck.
 //     Once you have both cards, ***console.log*** the values and suits of both cards.
 
-let deck_id = ""
-let card_1 = ""
-let card_2 = ""
+let deck_id = "" // store the deck_id details
+let card_1 = "" // store card 1 detials
+let card_2 = "" // store card 2 details
 
+// request new deck from DeckOfCardsAPI
 axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 .then(res => {
     deck_id = res.data.deck_id
+    // draw a card from the new deck
     return axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
 })
 .then(res => {
     card_1 = res.data.cards[0]
+    // draw another card from the new deck
     return axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`)
 })
 .then(res => {
     card_2 = (res.data.cards[0])
+    // log both card details
     console.log(`Deck ID: ${deck_id}. CARD 1: ${card_1.value} OF ${card_1.suit}. CARD 2: ${card_2.value} OF ${card_2.suit}`)
 })
 .catch(err => { console.log("Error", err)})
@@ -90,24 +103,30 @@ function getRandomRotation() {
 
 $(document).ready(function() {
 
-    let new_deck_id = ""
+    let new_deck_id = "" // store the deck_id details
 
+    // request a new deck from the DeckOfCardsAPI
     axios.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     .then(res => {
+        // store the deck id in the variable
         new_deck_id = res.data.deck_id
     })
     .catch(err => { console.log("Error", err)})
 
+    // append a "Draw Card" button to the dom
     $('.card_game').append('<button class="draw_card">Draw Card</button>');
 
+    // function runs when the "Draw Card" button is pressed
     $('.draw_card').on('click', function() {
         axios.get(`https://deckofcardsapi.com/api/deck/${new_deck_id}/draw/?count=1`)
             .then(res => {
-                const card_drawn = res.data.cards[0];
-                const rotation = getRandomRotation();
+                const card_drawn = res.data.cards[0]; // store card details
+                const rotation = getRandomRotation(); // store random rotaion that was generated
+                // append the card image to the web page
                 $('.card_game').append(`<img src="${card_drawn.image}" class="card_image" style="transform: translate(-50%, -50%) rotate(${rotation}deg);"/>`);
             })
             .catch(err => {
+                // log and append the error
                 console.error("Error drawing card:", err);
                 $('.card_game').append(`<p style="color:red;">No More Cards</p>`);
             });
@@ -120,34 +139,37 @@ $(document).ready(function() {
 
 // 2. Once you have names and URLs of all the pokemon, pick three at random and make requests to their URLs. Once those requests are complete, ***console.log*** the data for each pokemon.
 
-let pokemon_1 = {}
-let pokemon_2 = {}
-let pokemon_3 = {}
-let pokemon_1_info = {}
-let pokemon_2_info = {}
-let pokemon_3_info = {}
+let pokemon_1_url = "" // random pokemon 1 url 
+let pokemon_2_url = "" // random pokemon 2 url 
+let pokemon_3_url = "" // random pokemon 3 url 
+let pokemon_1_info = {} // pokemon 1 response object
+let pokemon_2_info = {} // pokemon 2 response object
+let pokemon_3_info = {} // pokemon 3 response object
 
+// get all pokemon from pokemon-species (these have images available)
 axios.get("https://pokeapi.co/api/v2/pokemon-species?limit=5000&offset=0")
 .then(res => { 
+    // store the object of all the pokemon data
     let all_pokemon = res.data.results
 
-    const random1 = Math.floor(Math.random() * 1302) + 1;
-    const random2 = Math.floor(Math.random() * 1302) + 1;
-    const random3 = Math.floor(Math.random() * 1302) + 1;
+    // generate three random nnumbers 
+    const random1 = Math.floor(Math.random() * Object.keys(all_pokemon).length) + 1;
+    const random2 = Math.floor(Math.random() * Object.keys(all_pokemon).length) + 1;
+    const random3 = Math.floor(Math.random() * Object.keys(all_pokemon).length) + 1;
 
-    pokemon_1 = all_pokemon[`${random1}`]
-    pokemon_2 = all_pokemon[`${random2}`]
-    pokemon_3 = all_pokemon[`${random3}`]
+    pokemon_1_url = all_pokemon[`${random1}`].url
+    pokemon_2_url = all_pokemon[`${random2}`].url
+    pokemon_3_url = all_pokemon[`${random3}`].url
 
-    return axios.get(pokemon_1.url)
+    return axios.get(pokemon_1_url)
 })
 .then( res => {
     pokemon_1_info = res.data
-    return axios.get(pokemon_2.url)
+    return axios.get(pokemon_2_url)
 })
 .then( res => {
     pokemon_2_info = res.data
-    return axios.get(pokemon_3.url)
+    return axios.get(pokemon_3_url)
 })
 .then( res => {
     pokemon_3_info = res.data
@@ -177,9 +199,10 @@ axios.get("https://pokeapi.co/api/v2/pokemon-species?limit=5000&offset=0")
 .then(res => { 
     let all_pokemon = res.data.results
 
-    const random1 = Math.floor(Math.random() * 1302) + 1;
-    const random2 = Math.floor(Math.random() * 1302) + 1;
-    const random3 = Math.floor(Math.random() * 1302) + 1;
+    // generate three random nnumbers 
+    const random1 = Math.floor(Math.random() * Object.keys(all_pokemon).length) + 1;
+    const random2 = Math.floor(Math.random() * Object.keys(all_pokemon).length) + 1;
+    const random3 = Math.floor(Math.random() * Object.keys(all_pokemon).length) + 1;
 
     pokemon_1_name = all_pokemon[`${random1}`].name
     pokemon_2_name = all_pokemon[`${random2}`].name
@@ -226,25 +249,35 @@ axios.get("https://pokeapi.co/api/v2/pokemon-species?limit=5000&offset=0")
 
 $(document).ready(function() {
 
+    // append "Random Pokemon" button to the web page
     $('.pokemon_cards').append('<button class="random_pokemon">Random Pokemon</button>');
 
-    let pokemon_list = {}
+    let pokemon_list = {} // store list of all pokemon available
 
+    // api call to get all pokemon
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=2000&offset=0")
     .then(res => { 
+        // save the response to the data
         pokemon_list = res.data.results
     })
+    // handle error
     .catch(err => {
         console.error("Error drawing card:", err);
         $('.pokemon_cards').append(`<p style="color:red;">An Error Occured</p>`);
     });
 
+    // function for when the "Random Pokemon" button is clicked
     $('.random_pokemon').on('click', function() {
 
-        const random = Math.floor(Math.random() * 1302) + 1;
+        // generate random number to choose a random pokemon
+        const random = Math.floor(Math.random() * Object.keys(pokemon_list).length) + 1;
+
+        // store the random pokemon name
         let random_pokemon_name = pokemon_list[`${random}`].name
+        // store the random pokemon image, id and description
         let random_pokemon_image, random_pokemon_id, random_pokemon_description
         
+        //get a  random pokemon
         axios.get(`https://pokeapi.co/api/v2/pokemon-species/${random_pokemon_name}`)
         .then(res => {
             random_pokemon_id = res.data.id
@@ -256,6 +289,7 @@ $(document).ready(function() {
             }
             return axios.get(`https://pokeapi.co/api/v2/pokemon/${random_pokemon_id}/`)
         })
+        //append the random pokemon to the web page
         .then(res => {
             random_pokemon_image = res.data.sprites.front_shiny
             $('.pokemon_cards').append(`<div class="pokemon_card">
@@ -264,6 +298,7 @@ $(document).ready(function() {
                 <p>${random_pokemon_description}</p>
                 </div>`);
         })
+        // handle error
         .catch(err => {
             console.error("Error generating pokemon:", err);
         });
